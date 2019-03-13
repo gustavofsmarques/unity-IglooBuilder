1715caa2591e6a1b1cb8575f749cbc77bb81aeb8
Gustavo Marques <gustavo.marques@godoo.com.br>
2019-03-13 00:28:43 -0300

    Adição da interface menu, credits.

---
+++ Assets/GameEvent/Script/Credits.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Credits : MonoBehaviour
+{
+    string name = "credits";
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene(name);
+    }
+}
