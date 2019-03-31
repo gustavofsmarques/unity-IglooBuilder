b791c436ad5f5b5c55cf8e181de49e97b5bf4cda
Gustavo Felipe <gustavo.fs.marques@gmail.com>
2019-03-31 00:15:48 +0000

    Hotfix. Adição do hyperlink para blog do unicluster

---
+++ Assets/GameEvent/Script/ManagementScene.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ManagementScene : MonoBehaviour
+{
+    public void ChangeScene(string scene) => SceneManager.LoadScene(scene);
+}
