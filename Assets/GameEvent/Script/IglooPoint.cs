d93c78352d137f13499dbc5a6b9a492779fc18ea
Danstorm <danieldavid0302@gmail.com>
2019-04-09 17:30:44 -0300

    Update: Gameplay, Igloo enter.

---
+++ Assets/GameEvent/Script/IglooPoint.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IglooPoint : MonoBehaviour
+{
+
+    [SerializeField] private string newLevel;
+
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(newLevel);
+        }
+
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
