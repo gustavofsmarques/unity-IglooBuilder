217dccef2aef4e7ca5eaa38f8c4d1d50c7d2c4b8
Gustavo Felipe <gustavo.fs.marques@gmail.com>
2019-04-14 20:34:35 -0300

    Desenvolvimento da cutscene de abertura

---
+++ Assets/GameEvent/Script/Cutscenes.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Cutscenes : MonoBehaviour
+{
+    public Text text;
+    public void EndCutscene()
+    {
+        ManagementScene managementScene = new ManagementScene();
+        managementScene.ChangeScene("menu");
+    }
+    public IEnumerator WriteText(string fulltext)
+    {
+        float delay = 0.030f;
+        string currentext = "";
+        text.text = "";
+
+        for (int i = 0; i < fulltext.Length+1; i++)
+        {
+            currentext = fulltext.Substring(0, i);
+            text.text = currentext;
+            yield return new WaitForSeconds(delay);
+        }
+
+    }
+}
