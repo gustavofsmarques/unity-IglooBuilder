629fdff035feae5c87a634dc96b6c7fce9c936b3
Danstorm <danieldavid0302@gmail.com>
2019-04-29 20:44:54 -0300

    Loading Transition

---
+++ Assets/GameEvent/Script/LoadingLevel.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadingLevel : MonoBehaviour
+{
+    public GameObject loadingScreen;
+    public Slider slider;
+    public Text progressText;
+    
+    public void loadLevel (int sceneIndex)
+    {
+
+        StartCoroutine(loadAsync(sceneIndex));
+
+    }
+
+    IEnumerator loadAsync (int sceneIndex)
+    {
+
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+
+        loadingScreen.SetActive(true);
+
+        while (!operation.isDone)
+        {
+            loadingScreen.SetActive(true);
+            float progress = Mathf.Clamp01(operation.progress / 0.9f);
+            slider.value = progress;
+            progressText.text = progress * 100f + "%";
+            Debug.Log(progress);
+            yield return null;
+                
+        }
+
+
+    }
+
+}
