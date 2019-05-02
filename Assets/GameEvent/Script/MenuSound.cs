ae9ccb4f203f0bc5db687990738dd1e96a41b2b2
Danstorm <danieldavid0302@gmail.com>
2019-05-01 21:22:40 -0300

    Update Menu

--- Assets/GameEvent/Script/MenuSound.cs
+++ Assets/GameEvent/Script/MenuSound.cs
@@ -6,26 +6,27 @@ using UnityEngine.UI;
 public class MenuSound : MonoBehaviour
 {
 
-    public GameObject SoundUI, SoundPanel, play, credits, sound, info;
+    public GameObject SoundUI, SoundPanel, play, credits, sound, info, tutorial;
 
     public void Pause()
     {
        
         SoundUI.SetActive(true);
-        play.SetActive(false);
         credits.SetActive(false);
         sound.SetActive(false);
         info.SetActive(false);
+
     }
 
     public void Resume()
     {
         
         SoundUI.SetActive(false);
-        play.SetActive(true);
+        play.SetActive(false);
         credits.SetActive(true);
         sound.SetActive(true);
         info.SetActive(true);
+        tutorial.SetActive(false);
     }
 
 }
---
+++ Assets/GameEvent/Script/MenuTutorial.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class MenuTutorial : MonoBehaviour
+{
+    public GameObject buttonplay, buttoninfo,credits,info,sound,infopainel;
+
+    public void disableinfo( )
+    {
+        buttonplay.SetActive(true);
+        buttoninfo.SetActive(false);
+    }
+
+    public void enableinfo( )
+    {
+
+        buttonplay.SetActive(false);
+        buttoninfo.SetActive(true);
+
+
+    }
+
+    public void backInfo()
+    {
+        buttonplay.SetActive(false);
+        buttoninfo.SetActive(false);
+        credits.SetActive(true);
+        sound.SetActive(true);
+        info.SetActive(true);
+        infopainel.SetActive(false);
+       
+    }
+
+}
